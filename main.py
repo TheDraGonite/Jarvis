062515d19f7ac68f7454c64a1b4f3b6ac71c87c1
Drago <dragonitetop@mail.ru>
2021-02-23 19:02:31 +0400

    123

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-"""Jarvis."""
+"""Джарвис 0.2"""
+import pyttsx3
+import speech_recognition as sr
+
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("")
+    r.pause_threshold=1
+    r.adjust_for_ambient_noise(source, duration=1)
+    audio = r.listen(source)
+
+try:
+    command = r.re
