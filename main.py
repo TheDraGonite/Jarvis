512ea82d211f81040611faacbc8f51faf0eff958
Drago <dragonitetop@mail.ru>
2021-02-27 02:10:21 +0400

    27.02 no sounds

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-"""Джарвис 0.2"""
+"""Jarvis 0.2"""
 import pyttsx3
 import speech_recognition as sr
 
 engine = pyttsx3.init()  # На всякий случай сделал engine
 r = sr.Recognizer()  # Звукоразпознавалка
 
-with sr.Microphone() as source:
-    print("Слушаю..")
+with sr.Microphone() as source:  # чтение микрофона
+    print("Слушаю...")
     r.pause_threshold = 1
     r.adjust_for_ambient_noise(source, duration=1)
     audio = r.listen(source, phrase_time_limit=5)
 
 try:
     recognized = r.recognize_google(audio, language='ru-RU')
-    print("Sphinx thinks you said", recognized)
+    print("Jarvis услышал -", recognized)
     if type(recognized) == list:
         recognized_string = " ".join(recognized)
         engine.say(recognized_string)
     else:
         engine.say(recognized)
 except sr.UnknownValueError:
-    print("Sphinx could not understand audio")
+    print("Jarvis не может распознать звук")
 except sr.RequestError as e:
-    print("Sphinx error; {0}".format(e))
+    print("Jarvis error; {0}".format(e))
